90502fd32dcffc8e4e3e38937aef1ce24c97503d
Joseph Topasna <josephtopasna@D-69-91-131-11.dhcp4.washington.edu>
2015-07-10 12:52:04 -0700

    modified all files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dojo-tooltip-solution
+
+Solution for INFO 343 Dojo API presentation exercise
---
+++ main.js
@@ -0,0 +1,4 @@
+// Load the Tooltip widget class
+require(["dijit/Tooltip",  "dojo/parser", "dojo/domReady!"], function(Tooltip, parser){
+        parser.parse();
+    });
